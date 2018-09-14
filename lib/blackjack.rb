cdec52e84aa1bc10cbf0f435ee092176d126e292
Gina Fedder <ginafedder@yahoo.com>
2018-09-14 22:10:53 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,3 +50,7 @@ end
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
+
+def runner
+  
+end
